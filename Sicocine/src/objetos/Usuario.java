d96082def4f950fdbeca8833666c27538779f5aa
Felipe Mello <felper3d@hotmail.com>
2011-05-05 15:45:40 -0300

    novos arquivos

---
+++ Sicocine/src/objetos/Usuario.java
@@ -0,0 +1,18 @@
+package objetos;
+
+public class Usuario {
+	private String nome;
+	private int registro;
+	private boolean ehAdministrador;
+	private String login;
+	private String senha;
+	
+	boolean vender(Sessao sessao){
+		
+		return false;
+	}
+	
+	void deslogar(){
+		
+	}
+}
