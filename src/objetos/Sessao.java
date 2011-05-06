74c253e1496bedfa09ae48368f8e44a5e60e2d1a
TrabCOO <lapis_lazuli91@hotmail.com>
2011-05-06 16:13:12 -0300

    Primeiro envio deste repositorio. Aqui vocês podem usar no eclipse

---
+++ src/objetos/Sessao.java
@@ -0,0 +1,86 @@
+package objetos;
+
+import java.sql.Date;
+
+public class Sessao {
+	private Filme filme;
+	private String horarioDeInicio;
+	private String horarioDeFim;
+	private Sala sala;
+	private double preco;
+	private boolean eh3d;
+	private Date duracao;
+	private int lugaresDisponiveis;
+	
+	Sessao(Filme filme, String inicio, String fim, Sala sala, double preco, boolean is3d, Date duracao, int disp){
+
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
+	public String getHorarioDeInicio() {
+		return horarioDeInicio;
+	}
+
+	public void setHorarioDeInicio(String horarioDeInicio) {
+		this.horarioDeInicio = horarioDeInicio;
+	}
+
+	public String getHorarioDeFim() {
+		return horarioDeFim;
+	}
+
+	public void setHorarioDeFim(String horarioDeFim) {
+		this.horarioDeFim = horarioDeFim;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public boolean isEh3d() {
+		return eh3d;
+	}
+
+	public void setEh3d(boolean eh3d) {
+		this.eh3d = eh3d;
+	}
+
+	public Date getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(Date duracao) {
+		this.duracao = duracao;
+	}
+
+	public int getLugaresDisponiveis() {
+		return lugaresDisponiveis;
+	}
+
+	public void setLugaresDisponiveis(int lugaresDisponiveis) {
+		this.lugaresDisponiveis = lugaresDisponiveis;
+	}
+	
+	
+	
+	
+}
