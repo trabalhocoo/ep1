9fae7b6f5a05e71fc94e3fc40853055c98451d27
Eric Kureck <kureck@gmail.com>
2011-05-16 07:25:32 -0300

    insercao de admin e interface da elaine

--- src/objetos/Administrador.java
+++ src/objetos/Administrador.java
@@ -8,97 +8,124 @@ import java.util.ListIterator;
 import java.util.TreeSet;
 
 import visualizacao.Exibir;
+import visualizacao.InterfaceCinema;
 
 import banco.Banco;
 
 public class Administrador extends Usuario implements Serializable {
 	
 	
-	Administrador (String nomenome, int registroregistro, boolean admadm, String loginlogin, String senhasenha){
-		super(nomenome, registroregistro, true, loginlogin, senhasenha);
+	Administrador (String nome, int registro, boolean adm, String login, String senha){
+		super(nome, adm, login, senha);
 	}
 		
-	void addSessao(Filme filme, Date hora, Sala sala, double preco, boolean is3d){
+	void adicionarSessao(Filme filme, Date hora, Sala sala, double preco, boolean is3d){
 		long fim_horario = (long) (hora.getTime() + filme.getDuracao()) ;//falta nao sei como fazer isso teria que somar o horario do inicio +tempo de filme 
 		long duracao_final = fim - hora.getTime();
 		Date duracao = new Date (duracao_final);
-		
-		Sessao _sessao = new Sessao (filme, hora, fim, sala, preco,duracao, sala.getCapacidade());
+		//nao sei como manipular horas
+		Sessao sessaoNova = new Sessao (filme, hora, fim, sala, preco,duracao, sala.getCapacidade());
 	}
 		
-	void modificarSessão(Sessao sessao, Filme filme, Date hora, Sala sala, double Preco, boolean 3d){
-		//esperar 
+	void alterarSessão(){
+		
+		//Filme filme, Date inicio, long fim, Sala sala, double preco, 
+		//Date duracao, int disp
+		Sessao sessaoAAlterar = InterfaceCinema.alterarSessao();
+		Banco.modificarSessao(sessaoAAlterar, filmeAAlterar, sessaoAAlterar.getHorarioDeInicio(), sessaoAAlterar.getHorarioDeFim(), sala, sessao.getPreco());
 	}
 	
-	boolean removerSessão(Sessao sessao){
+	boolean removerSessão(){
 		// se der tem que colocar no registro
-		boolean removeu = false;
-		removeu = Banco.removerSessao(Sessao sessao);
-		return removeu;
+		//NAO SEI COMO FAZER ISSO
+		Sessao sessao = InterfaceCinema.removerSessao();
+		//falta remover sessao no interface cinema
+		return Banco.removerSessao(Sessao sessao);
 	}
 	
-	void addSala(int capacidade, int numero, boolean is3d){
-		Sala _sala = new Sala (capacidade,numero,is3d);
+	public static void adicionarSala(){
+		ArrayList resposta = InterfaceCinema.adicionarSala();
+		//int capacidade, int numero, boolean is3d
+		int capacidade = (Integer) resposta.get(0);
+		int numero = (Integer) resposta.get(1);
+		boolean eh3d = (Boolean) resposta.get(2);
+		Sala salaNova = new Sala(capacidade, numero, eh3d);
+		Banco.addSala(salaNova);
 	}
 	
-	void modificarSala(Sala sala, int capacidade, int numero, boolean 3d){
-		ArrayList resposta = new 
+	void alterarSala(){
+		ArrayList <Object> resposta = InterfaceCinema.alterarSala();
+		Sala sala = (Sala) resposta.get(0);
+		int capacidade = (Integer) resposta.get(1); 
+		int numero = (Integer) resposta.get(2);
+		boolean eh3d = (Boolean) resposta.get(3);
+		Banco.modificarSala(sala, capacidade, numero, eh3d);
 	}
 	
 	void exibirUsuarios(){//listar os usuarios existentes
         TreeSet <Usuario> listaUsuarios = Banco.getUsuarios();
         Exibir.exibirUsuarios(listaUsuarios);
 }
-	
-	boolean addCaixa(int numero){
+	boolean adicionarCaixa(){
 		Caixa caixaNova = new Caixa();
-		//falta metodo adicionar caixa
-		Banco.addCaixa(caixaNova);
+		//falta metodo adicionar caixa OK
+	return	Banco.addCaixa(caixaNova);
 	}
 	
 	boolean removerCaixa(){
-		//remover caixa
-		Caixa caixa = InterfaceCinema.removerCaixa();
+		int caixa = InterfaceCinema.removerCaixa();
+		//falta removerCaixa(int) no banco OK
 		return Banco.removerCaixa(caixa);
 	}
 	
-	void addFilme(){
-		ArrayList resposta = InterfaceCinema.adicionarAtributosDoFilme();
-		Filme filmeNovo = new Filme (resposta.get(0), resposta.get(1), resposta.get(2), resposta.get(3), resposta.get(4), resposta.get(5), resposta.get(6));
+	void adicionarFilme(){
+		ArrayList <Object> resposta = InterfaceCinema.adicionarFilme();
+		int faixa = (Integer) resposta.get(1);
+		boolean eh3d = (Boolean) resposta.get(7);
+		Date duracao = (Date) resposta.get(2);
+		Filme filmeNovo = new Filme (resposta.get(0).toString(), faixa, duracao, resposta.get(3).toString(), resposta.get(4).toString(), resposta.get(5).toString(), resposta.get(6).toString(), eh3d);
 		Banco.adicionarFilme (filmeNovo);
 	}
-	//String nome, int faixa, Date duracao, String diretor, String sinopse, String genero, String estreia, boolean is3d)
-	void modificarFilme(){
-		ArrayList resposta = InterfaceCinema.alterarAtributosDoFilme();
-		//falta método obter filme
+	
+	void alterarFilme(){
+		ArrayList resposta = InterfaceCinema.alterarFilme();
+		//falta método obter filme OK
 		Filme filmeAAlterar= Banco.obterFilme(resposta.get(0));
 		Banco.modificarFilme (filmeAAlterar, resposta.get(1), resposta.get(2), resposta.get(3), resposta.get(4), resposta.get(5);
 	}
 	
-	boolean removerFilme(Filme filme){
-		return Banco.removerFilme (filme);
+	boolean removerFilme(){
+		Filme filme = InterfaceCinema.removerFilme();
+		return Banco.removerFilme(filme);
 	}
 	
-	void addUsuario(){
-		//falta metodo
-		ArrayList resposta = InterfaceCinema.addUsuario();
-		Usuario novoUsuario = new Usuario (resposta.get(0),resposta.get(1), resposta.get(2), resposta.get(3));
+	void adicionarUsuario(){
+		ArrayList resposta = InterfaceCinema.adicionarUsuario();
+		//String nomenome, boolean admadm, String loginlogin, String passwd
+		boolean adm = (Boolean) resposta.get(1);
+		String login = "0";
+		if (adm == true){
+			login = "1";
+		}
+		Usuario novoUsuario = new Usuario (resposta.get(0).toString(), adm, login, resposta.get(3).toString());
 		Banco.addUsuario (novoUsuario);
 	}
 	
-	String nomenome, boolean admadm, String loginlogin, String passwd
-	
 	//teoricamente eu vou tirar os parametros de todos os métodos
 	boolean removerUsuario(){
-		// falta metodo
 		Usuario usuario = InterfaceCinema.removerUsuario();
 		return Banco.removerUsuario(usuario);
 	}
 
-	void modificarUsuario(){
-		ArrayList<E> resposta = InterfaceCinema.alterarAtributosDoUsuario();
-		Usuario usuarioAAlterar = Banco.obterUsuario(resposta.get(1));
-		Banco.modificarUsuario (usuarioAAlterar, resposta.get(0), resposta.get(2),resposta.get(3), resposta.get(4));
+	void alterarUsuario(){
+		ArrayList <Object> resposta = InterfaceCinema.alterarUsuario();
+		boolean ehAdmin = (Boolean)resposta.get(2);
+		String login = "0";
+		if (ehAdmin == true){
+			login = "1";
+		}
+		Usuario usuarioAAlterar = (Usuario) resposta.get(1);
+		Banco.modificarUsuario (usuarioAAlterar,resposta.get(1).toString(), ehAdmin,login , resposta.get(3).toString());
 	}
 
 }
