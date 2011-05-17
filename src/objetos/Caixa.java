8ba965735fc1cfaa49a65b711841805b60918b9f
TrabCOO <lapis_lazuli91@hotmail.com>
2011-05-17 20:10:30 -0300

    adicionei o Caixa.java

---
+++ src/objetos/Caixa.java
@@ -0,0 +1,34 @@
+package objetos;
+
+
+
+public class Caixa{
+	private static int numeroDeCaixas =0;
+	private int numeroDaCaixa;
+	private double dinheiro;
+	
+	public double getDinheiro() {
+		return dinheiro;
+	}
+
+	public int getNumeroDaCaixa() {
+		return numeroDaCaixa;
+	}
+
+	public void setNumeroDaCaixa(int numeroDaCaixa) {
+		this.numeroDaCaixa = numeroDaCaixa;
+	}
+
+	public void setDinheiro(double dinheiro) {
+		this.dinheiro = dinheiro;
+	}
+
+	public Caixa (){
+		numeroDaCaixa = numeroDeCaixas++;
+	}
+	
+	public int getNumCaixa (){
+		return numeroDaCaixa;
+	}
+	
+}
