30c697ec862bd0217a38b6b472db950ef2b64ecd
unknown <Gisele@.(none)>
2011-06-12 19:47:09 -0300

    interfaceGrafica

---
+++ src/interfaceGrafica/LoginSenhaFrame.java
@@ -0,0 +1,127 @@
+package interfaceGrafica;
+import javax.swing.*;
+import objetos.*;
+import java.awt.*;
+import banco.Banco;
+import controlador.*;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class LoginSenhaFrame extends JFrame{
+	private JTextField textField;
+	private JPasswordField passwordField;
+	
+	public LoginSenhaFrame()
+	{
+		this.setSize(250,250);
+		this.setLocation(200,200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DIGITE SEU LOGIN E SENHA:");
+		lblNewLabel.setBounds(40, 39, 213, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblLogin = new JLabel("Login:");
+		lblLogin.setBounds(40, 79, 46, 14);
+		getContentPane().add(lblLogin);
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setBounds(40, 110, 46, 14);
+		getContentPane().add(lblSenha);
+		
+		textField = new JTextField();
+		textField.setBounds(85, 76, 86, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(85, 107, 86, 20);
+		getContentPane().add(passwordField);
+		//senha = passwordField.getPassword().toString();
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setBounds(95, 141, 63, 20);
+		getContentPane().add(btnOk);
+		
+		JLabel label = new JLabel("");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		label.setForeground(Color.BLACK);
+		label.setBounds(10, 170, 222, 14);
+		getContentPane().add(label);
+		
+		final JLabel lblNewLabel_1 = new JLabel("");//vazio por enquanto
+		lblNewLabel_1.setBounds(40, 172, 200, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		
+		
+		
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//teria que logar e perguntar qual é o usuario que logou e depois decidir se aparece o usuarioframe ou adminframe
+				boolean logou = false;
+				boolean logado = Controlador.isLogado();
+				boolean tipoUser;
+				boolean reiniciar = true;
+				Controlador.setReiniciar(reiniciar);
+				Banco.recuperarDados();
+				int login = Integer.parseInt(textField.getText());
+				String senha = passwordField.getText();
+				//System.out.println (tipoUser);
+				try{
+					logou = Controlador.logar(login, senha);
+				}
+				catch (Exception e){
+					System.out.print(e);
+				}
+				tipoUser = Controlador.tipoUsuario();
+				logado = Controlador.isLogado();
+				if(!logou){
+					lblNewLabel_1.setOpaque(false);
+					lblNewLabel_1.setForeground(Color.red);
+					textField.setText("");
+					passwordField.setText("");
+					lblNewLabel_1.setText("O login e/ou senha inválidos");
+				}
+				else {
+					if (tipoUser){
+						//incluir sua janela
+					} else {
+						UsuarioFrame usfr = new UsuarioFrame();
+						usfr.setVisible(true);
+					}
+					hideIt();
+				}
+			}
+		});
+		
+	}
+	
+
+	// Makes the frame visible.
+	public void showIt(){
+		this.setVisible(true);
+	}
+	/*
+	// Makes the frame visible and sets the title text.
+	public void showIt(String title){
+	this.setTitle(title);
+	this.setVisible(true);
+	}
+	
+	// Makes the frame visible and sets the title text
+	// and the position of the window.
+	public void showIt(String title,int x, int y){
+	this.setTitle(title);
+	this.setLocation(x,y);
+	this.setVisible(true);
+	}
+	*/
+	
+	// Makes the frame invisible.
+	public void hideIt(){
+	this.setVisible(false);
+	}
+}
---
+++ src/interfaceGrafica/UsuarioFrame.java
@@ -0,0 +1,69 @@
+package interfaceGrafica;
+import javax.swing.*;
+
+import objetos.Usuario;
+
+import java.awt.*;
+
+import controlador.Controlador;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class UsuarioFrame extends JFrame{
+	private static Usuario usuarioLogado;
+	
+	
+	public UsuarioFrame (){
+		this.setSize(550,380);
+		this.setLocation(200,200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("LISTAR FILMES");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				usuarioLogado.exibirFilmes();
+			}
+		});
+		btnNewButton.setBounds(54, 70, 142, 32);
+		getContentPane().add(btnNewButton);
+		
+		JButton button = new JButton("LISTAR SALAS");
+		button.setBounds(54, 143, 142, 32);
+		getContentPane().add(button);
+		
+		JButton button_1 = new JButton("LISTAR SESSÕES");
+		button_1.setBounds(54, 217, 142, 32);
+		getContentPane().add(button_1);
+		
+		JButton button_2 = new JButton("VENDER INGRESSO");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		button_2.setBounds(297, 70, 142, 32);
+		getContentPane().add(button_2);
+		
+		JButton button_3 = new JButton("DESLOGAR");
+		button_3.setBounds(297, 143, 142, 32);
+		getContentPane().add(button_3);
+		
+		JButton button_4 = new JButton("SAIR DO PROGRAMA");
+		button_4.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		button_4.setBounds(297, 217, 142, 32);
+		getContentPane().add(button_4);
+	}
+	
+	// Makes the frame visible.
+	public void showIt(){
+		this.setVisible(true);
+	}
+	
+	// Makes the frame invisible.
+	public void hideIt(){
+	this.setVisible(false);
+	}
+}
