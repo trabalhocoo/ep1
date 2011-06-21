c09044df27ae7fc2fdc21553dd1d8deaf32d0d67
unknown <Firewall@.(none)>
2011-06-21 15:24:51 -0300

    editar usuario resistente

--- src/interfaceGrafica/EditarUsuarioFrame.java
+++ src/interfaceGrafica/EditarUsuarioFrame.java
@@ -1,4 +1,5 @@
 package interfaceGrafica;
+
 import java.awt.Color;
 import javax.swing.*;
 
@@ -13,42 +14,43 @@ import java.util.ArrayList;
 import objetos.Administrador;
 import objetos.Usuario;
 
-public class EditarUsuarioFrame extends JFrame{
+public class EditarUsuarioFrame extends JFrame {
 	private static Usuario usuarioLogado;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JComboBox comboBox;
-	
+
 	public EditarUsuarioFrame(final Usuario usrLogado) {
 		usuarioLogado = usrLogado;
-		final Administrador admin = (Administrador)usuarioLogado;
-		this.setSize(550,380);
-		this.setLocation(200,200);
+		final Administrador admin = (Administrador) usuarioLogado;
+		this.setSize(550, 380);
+		this.setLocation(200, 200);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setBackground(Color.white); //mudar a cor
-		
-		JLabel lblDigiteONumero = new JLabel("Digite o numero do registro de usuario que deseja alterar:");
+		getContentPane().setBackground(Color.white); // mudar a cor
+
+		JLabel lblDigiteONumero = new JLabel(
+				"Digite o numero do registro de usuario que deseja alterar:");
 		lblDigiteONumero.setBounds(23, 85, 483, 14);
 		getContentPane().add(lblDigiteONumero);
-		
+
 		textField = new JTextField();
 		textField.setBounds(387, 82, 53, 20);
 		getContentPane().add(textField);
 		textField.setColumns(10);
-		
-		String[] options = {"", "Admin", "Usuario"};   
+
+		String[] options = { "", "Admin", "Usuario" };
 		comboBox = new JComboBox(options);
-		comboBox.setSelectedIndex(0); 
+		comboBox.setSelectedIndex(0);
 		comboBox.setBackground(Color.white);
-		//comboBox.addActionListener();  
+		// comboBox.addActionListener();
 		comboBox.setBounds(97, 189, 104, 20);
 		getContentPane().add(comboBox);
-		
-		final JLabel lblNewLabel_2 = new JLabel("");//vazio por enquanto
+
+		final JLabel lblNewLabel_2 = new JLabel("");// vazio por enquanto
 		lblNewLabel_2.setBounds(23, 50, 298, 14);
 		getContentPane().add(lblNewLabel_2);
-		
+
 		JButton btnLimparCampos = new JButton("Limpar campos");
 		btnLimparCampos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -60,7 +62,7 @@ public class EditarUsuarioFrame extends JFrame{
 		});
 		btnLimparCampos.setBounds(209, 259, 123, 23);
 		getContentPane().add(btnLimparCampos);
-		
+
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.setBounds(364, 259, 123, 23);
 		btnVoltar.addActionListener(new ActionListener() {
@@ -72,20 +74,21 @@ public class EditarUsuarioFrame extends JFrame{
 		});
 		getContentPane().setLayout(null);
 		getContentPane().add(btnVoltar);
-		
+
 		JButton btnAdicionar = new JButton("Alterar");
 		btnAdicionar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if (textField_1.getText().equals("") || textField_2.getText().equals("") ){
+				if (textField_1.getText().equals("")
+						|| textField_2.getText().equals("")) {
 					lblNewLabel_2.setOpaque(false);
 					lblNewLabel_2.setForeground(Color.red);
-					lblNewLabel_2.setText("Por favor, preencher todos os campos.");
-				}
-				else {	
+					lblNewLabel_2
+							.setText("Por favor, preencher todos os campos.");
+				} else {
 					boolean alterou = false;
 					lblNewLabel_2.setText("");
-					try{
-						
+					try {
+
 						// 0: usuario, 1: nome, 2: ehadmin, 3: senha
 						int registro = Integer.parseInt(textField.getText());
 						Usuario usuarioAAlterar = Banco.obterUsuario(registro);
@@ -96,66 +99,78 @@ public class EditarUsuarioFrame extends JFrame{
 						int option = comboBox.getSelectedIndex();
 						String nome = textField_1.getText();
 						String senha = textField_2.getText();
-						String permissao = (String)comboBox.getSelectedItem();
+						String permissao = (String) comboBox.getSelectedItem();
 						boolean permissaoUsuario = false;
-						if (permissao.equals("Admin")) permissaoUsuario = true;
+						if (permissao.equals("Admin"))
+							permissaoUsuario = true;
 						arrayListDeUsuario.add(nome);
 						arrayListDeUsuario.add(permissaoUsuario);
 						arrayListDeUsuario.add(senha);
-						//System.out.println (nome + " " + permissao + " " + senha);
+						// System.out.println (nome + " " + permissao + " " +
+						// senha);
 						alterou = admin.alterarUsuario(arrayListDeUsuario);
-					
-					} catch (Exception e){
+
+					} catch (Exception e) {
 						System.out.println(e);
 					}
-					if (alterou){
+					if (alterou) {
 						lblNewLabel_2.setText("Usuario alterado com sucesso.");
 						textField_1.setText("");
 						comboBox.setSelectedIndex(0);
 						textField_2.setText("");
 						textField.setText("");
-						
-					}
-					else
+
+					} else
 						lblNewLabel_2.setText("Usuario nao encontrado.");
-						textField.setText("");
+					textField.setText("");
 				}
 			}
 		});
 		btnAdicionar.setBounds(52, 259, 123, 23);
 		getContentPane().add(btnAdicionar);
-		
-		
+
 		JButton btnNewButton = new JButton("OK");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				lblNewLabel_2.setOpaque(false);
 				lblNewLabel_2.setForeground(Color.red);
-				if (textField.getText().equals("")){
-					lblNewLabel_2.setText("Digite o numero de registro do usuário.");
-				}
-				else{
+				if (textField.getText().equals("")) {
+					lblNewLabel_2
+							.setText("Digite o numero de registro do usuário.");
+				} else {
 					boolean alterou = false;
 					lblNewLabel_2.setText("");
-					try{
+					try {
 						int registro = Integer.parseInt(textField.getText());
-						Usuario usuarioAAlterar = Banco.obterUsuario(registro);
-					
-						if (usuarioAAlterar == null){
-							lblNewLabel_2.setText("Usuario nao encontrado.");
+						if (registro < 1) {
+							lblNewLabel_2
+									.setText("Digite apenas valores maiores que zero.");
 							textField.setText("");
 							textField_1.setText("");
 							textField_2.setText("");
+						} else {
+							Usuario usuarioAAlterar = Banco
+									.obterUsuario(registro);
+
+							if (usuarioAAlterar == null) {
+								lblNewLabel_2
+										.setText("Usuario nao encontrado.");
+								textField.setText("");
+								textField_1.setText("");
+								textField_2.setText("");
+							} else {
+								textField_1.setText(usuarioAAlterar.getNome());
+								textField_2.setText(usuarioAAlterar.getSenha());
+								if (usuarioAAlterar.isEhAdministrador())
+									comboBox.setSelectedIndex(1);
+								else
+									comboBox.setSelectedIndex(2);
+							}
 						}
-						else {
-							textField_1.setText(usuarioAAlterar.getNome());
-							textField_2.setText(usuarioAAlterar.getSenha());
-							if (usuarioAAlterar.isEhAdministrador())
-								comboBox.setSelectedIndex(1);
-							else 
-								comboBox.setSelectedIndex(2);
-						}
-					} catch (Exception e){
+					} catch (NumberFormatException e) {
+						lblNewLabel_2
+								.setText("Digite apenas caracteres numéricos.");
+					} catch (Exception e) {
 						System.out.println(e);
 					}
 				}
@@ -163,60 +178,56 @@ public class EditarUsuarioFrame extends JFrame{
 		});
 		btnNewButton.setBounds(448, 81, 58, 23);
 		getContentPane().add(btnNewButton);
-		
+
 		JLabel lblAlltereOsCamoos = new JLabel("Altere os campos desejados:");
 		lblAlltereOsCamoos.setBounds(23, 119, 239, 14);
 		getContentPane().add(lblAlltereOsCamoos);
-		
+
 		JLabel lblNome = new JLabel("Nome:");
 		lblNome.setBounds(23, 167, 46, 14);
 		getContentPane().add(lblNome);
-		
+
 		JLabel label = new JLabel("Permissao:");
 		label.setBounds(23, 192, 89, 14);
 		getContentPane().add(label);
-		
+
 		JLabel label_1 = new JLabel("Senha:");
 		label_1.setBounds(23, 217, 78, 14);
 		getContentPane().add(label_1);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(97, 164, 361, 20);
 		getContentPane().add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
 		textField_2.setBounds(97, 214, 361, 20);
 		getContentPane().add(textField_2);
-		
-		
-		
-		
+
 	}
-	
-	
+
 	// Makes the frame visible.
-	public void showIt(){
+	public void showIt() {
 		this.setVisible(true);
 	}
-	
+
 	// Makes the frame invisible.
-	public void hideIt(){
-	this.setVisible(false);
+	public void hideIt() {
+		this.setVisible(false);
 	}
-	
+
 	// Makes the frame visible and sets the title text.
-	public void showIt(String title){
-	this.setTitle(title);
-	this.setVisible(true);
+	public void showIt(String title) {
+		this.setTitle(title);
+		this.setVisible(true);
 	}
-	
+
 	// Makes the frame visible and sets the title text
 	// and the position of the window.
-	public void showIt(String title,int x, int y){
-	this.setTitle(title);
-	this.setLocation(x,y);
-	this.setVisible(true);
+	public void showIt(String title, int x, int y) {
+		this.setTitle(title);
+		this.setLocation(x, y);
+		this.setVisible(true);
 	}
 }
--- src/interfaceGrafica/RemoverSessaoFrame.java
+++ src/interfaceGrafica/RemoverSessaoFrame.java
@@ -1,4 +1,5 @@
 package interfaceGrafica;
+
 import javax.swing.*;
 import java.util.Date;
 import java.awt.Color;
@@ -21,65 +22,73 @@ import objetos.Usuario;
 public class RemoverSessaoFrame extends JFrame {
 	private static Usuario usuarioLogado;
 	private JTextField codSessao;
-	
+
 	public RemoverSessaoFrame(Usuario usuarioLogado) {
 		this.usuarioLogado = usuarioLogado;
-		final Administrador admin = (Administrador)usuarioLogado;
-		this.setSize(550,380);
-		this.setLocation(200,200);
+		final Administrador admin = (Administrador) usuarioLogado;
+		this.setSize(550, 380);
+		this.setLocation(200, 200);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
-		getContentPane().setBackground(Color.white); //mudar a cor
-		
+		getContentPane().setBackground(Color.white); // mudar a cor
+
 		final JLabel lblErro = new JLabel("");// vazio por enquanto
 		lblErro.setBounds(22, 60, 464, 14);
 		getContentPane().add(lblErro);
-		
-		JLabel lblDigiteONumero = new JLabel("Escolha o numero da sessao que deseja remover:");
+
+		JLabel lblDigiteONumero = new JLabel(
+				"Escolha o numero da sessao que deseja remover:");
 		lblDigiteONumero.setBounds(22, 38, 288, 14);
 		getContentPane().add(lblDigiteONumero);
-		
-		//campo para preencher o numero da sessao
+
+		// campo para preencher o numero da sessao
 		codSessao = new JTextField();
 		codSessao.setBounds(320, 35, 86, 20);
 		getContentPane().add(codSessao);
 		codSessao.setColumns(10);
-		
+
 		JButton btnOK = new JButton("OK");
 		btnOK.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				lblErro.setOpaque(false);
 				lblErro.setForeground(Color.red);
 				System.out.println(codSessao.getText());
-				if(codSessao.getText().equals("")){
+				if (codSessao.getText().equals("")) {
 					lblErro.setText("Por favor informar a sessao que deseja remover.");
-				}
-				else {
-					lblErro.setText("");
-					int numeroDaSessao = Integer.parseInt(codSessao.getText());
-					Sessao sessaoARemover = Banco.obterSessao(numeroDaSessao);
-					if (sessaoARemover!= null){
-					if (sessaoARemover.getVendeu()== true && sessaoARemover.getHorarioDeInicio().getTime().after(new Date())){
-						lblErro.setText("Sessao não pode ser removida pois já foi vendido ingresso.");
+				} else {
+					try {
+						lblErro.setText("");
+						int numeroDaSessao = Integer.parseInt(codSessao
+								.getText());
+						Sessao sessaoARemover = Banco
+								.obterSessao(numeroDaSessao);
+						if (sessaoARemover != null) {
+							if (sessaoARemover.getVendeu() == true
+									&& sessaoARemover.getHorarioDeInicio()
+											.getTime().after(new Date())) {
+								lblErro.setText("Sessao não pode ser removida pois já foi vendido ingresso.");
+							} else {
+								boolean removeu = admin.removerSessao(admin,
+										numeroDaSessao);
+								if (removeu) {
+									lblErro.setText("Sessao removida com sucesso.");
+								} else
+									lblErro.setText("Ocorreu um problema.Tente novamente.");
+							}
+						} else if(numeroDaSessao < 1){
+							lblErro.setText("O número da sessão deve ser maior que 1.");
+						} else
+							lblErro.setText("Sessao nao encontrada.");
+					} catch (NumberFormatException e) {
+						lblErro.setText("Digite apenas valores numéricos!");
 					}
-					else {
-						boolean removeu = admin.removerSessao(admin,numeroDaSessao);
-						if (removeu){
-						lblErro.setText("Sessao removida com sucesso.");
-					}
-					else
-						lblErro.setText("Ocorreu um problema.Tente novamente.");
-					}
-					}
-					else
-						lblErro.setText("Sessao nao encontrada.");
 				}
 			}
 		});
 		btnOK.setBounds(416, 34, 73, 23);
 		getContentPane().add(btnOK);
-		
-		//voltar
+
+		// voltar
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -90,34 +99,30 @@ public class RemoverSessaoFrame extends JFrame {
 		});
 		btnVoltar.setBounds(387, 259, 89, 23);
 		getContentPane().add(btnVoltar);
-		
-		
 
-		
-		
 	}
-	
+
 	// Makes the frame visible.
-	public void showIt(){
+	public void showIt() {
 		this.setVisible(true);
 	}
-	
+
 	// Makes the frame invisible.
-	public void hideIt(){
+	public void hideIt() {
 		this.setVisible(false);
 	}
-	
+
 	// Makes the frame visible and sets the title text.
-	public void showIt(String title){
+	public void showIt(String title) {
 		this.setTitle(title);
 		this.setVisible(true);
 	}
-	
+
 	// Makes the frame visible and sets the title text
 	// and the position of the window.
-	public void showIt(String title,int x, int y){
+	public void showIt(String title, int x, int y) {
 		this.setTitle(title);
-		this.setLocation(x,y);
+		this.setLocation(x, y);
 		this.setVisible(true);
 	}
 }
